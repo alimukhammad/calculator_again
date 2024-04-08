924cff2b2320619f9931338b7ac0d2eb9048e280
sardor <alimukhammad1@gmail.com>
2024-04-07 23:58:39 -0400

    calculator

---
+++ home.js
@@ -0,0 +1,46 @@
+
+const calculator = document.querySelector('.calculator'); //first element that matches the selector element
+const keys = calculator.querySelector('.calculator__key'); //first element with in calculator that matches the selector element
+const display = calculator.querySelector('.calculator__display'); //first element with in calculator that matches the selector element
+
+document.addEventListener('click', e => {
+    const key = e.target; 
+    const action = key.dataset.action; 
+    
+    if(e.target.matches('button')) {
+        if(action){
+            if(action === 'clear'){
+                clearDisplay();
+            }else if(action === 'calculate'){
+                calculate();
+            }else{
+                displayValue(key.textContent);
+            }
+        }else{
+            displayValue(key.textContent);
+        }
+        
+    }
+});
+
+function displayValue(value){
+    const displayField = document.getElementById('displayField');
+    if (value === '×') {
+        displayField.value += '*';
+    } else if (value === '÷') {
+        displayField.value += '/';
+    } else {
+        displayField.value += value;
+    }
+}
+
+function clearDisplay(){
+    const displayField = document.getElementById('displayField');
+    displayField.value = '';
+}
+
+function calculate(){
+    const displayField = document.getElementById('displayField');
+    displayField.value = eval(displayField.value);
+}
+
